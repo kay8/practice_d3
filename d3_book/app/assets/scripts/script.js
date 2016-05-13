a7732bd038bc8ef2ed8c17d74ed38857eec9495d
kay8 <kay.yamagishi.work@gmail.com>
2016-05-12 23:40:15 -0700

    data from csv

--- d3_book/app/assets/scripts/script.js
+++ d3_book/app/assets/scripts/script.js
@@ -1,26 +1,37 @@
 (function() {
 
-  var data = [300, 130, 5, 60, 240];
+  //var data = [300, 130, 5, 60, 240];
 
-  d3.select("#practice1")
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr({
-      x: 0,
-      y: function(d,i) {return i * 25;},
-      width: "0px",
-      height: "20px"
-    })
-    .transition()
-    .delay(function(d,i) {
-      return i * 500;
-    })
-    .duration(500)
-    .attr({
-      width: function(d,i) {return d + "px";}
-    });
+  d3.csv("assets/data/data.csv", function(error, data) {
+    var data1 = [];
+    var i,
+        dataNum = data.length;
+    for (i = 0; i < dataNum; i++) {
+      data1.push(data[i].item1);
+    }
+
+
+    d3.select("#practice1")
+      .selectAll("rect")
+      .data(data1)
+      .enter()
+      .append("rect")
+      .attr({
+        x: 0,
+        y: function(d,i) {return i * 25;},
+        width: "0px",
+        height: "20px"
+      })
+      .transition()
+      .delay(function(d,i) {
+        return i * 500;
+      })
+      .duration(500)
+      .attr({
+        width: function(d,i) {return d + "px";}
+      });
+
+  });
 
   // d3.select("#practice1")
   //   .selectAll("rect")
