895383b739d22ac29b6ee7bddd33fb31bbe0a120
kay8 <kay.yamagishi.work@gmail.com>
2016-05-13 23:48:36 -0700

    finish section 2

--- d3_book/app/assets/scripts/script.js
+++ d3_book/app/assets/scripts/script.js
@@ -1,19 +1,18 @@
 (function() {
 
-  //var data = [300, 130, 5, 60, 240];
+  var dataset = [];
 
   d3.csv("assets/data/data.csv", function(error, data) {
-    var data1 = [];
     var i,
         dataNum = data.length;
     for (i = 0; i < dataNum; i++) {
-      data1.push(data[i].item1);
+      dataset.push(data[i].item1);
     }
 
 
     d3.select("#practice1")
       .selectAll("rect")
-      .data(data1)
+      .data(dataset)
       .enter()
       .append("rect")
       .attr({
@@ -21,6 +20,12 @@
         y: function(d,i) {return i * 25;},
         width: "0px",
         height: "20px"
+      })
+      .on('click', function() {
+        d3.select(this)
+        .style({
+          fill: 'cyan'
+        });
       })
       .transition()
       .delay(function(d,i) {
@@ -39,7 +44,7 @@
       .append("g")
       .attr({
         class: "axis",
-        transform: "translate(10, " + ((1 + data1.length) * 20 + 5) + ")"
+        transform: "translate(10, " + ((1 + dataset.length) * 20 + 5) + ")"
       })
 
       .call(d3.svg.axis()
@@ -49,33 +54,19 @@
 
   });
 
-  // d3.select("#practice1")
-  //   .selectAll("rect")
-  //   .data(data)
-  //   .enter()
-  //   .append("rect")
-  //   .attr({
-  //     x: 0,
-  //     y: function(d,i) {return i * 25;},
-  //     width: function(d,i) {return d + "px";},
-  //     height: "20px"
-  //   })
-  //   .on('click', function() {
-  //     d3.select(this)
-  //       .style("fill", "cyan");
-  //   });
+
 
   d3.select(".js-update").on('click', function() {
 
     var i,
-        dataNum = data.length;
+        dataNum = dataset.length;
     for (i = 0; i < dataNum; i++) {
-      data[i] = Math.floor(Math.random() * 320);
+      dataset[i] = Math.floor(Math.random() * 320);
     }
 
     d3.select("#practice1")
       .selectAll("rect")
-      .data(data)
+      .data(dataset)
       .transition()
       .attr({
         width: function(d,i) {return d + "px";}
